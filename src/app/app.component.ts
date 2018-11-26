ffe3330c36fb05015601f93cb69bacab9430dc34
Avram Virgil <av.virgil@gmail.com>
2018-11-26 10:35:05 +0000

    Angular 7 web component example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'greet-comp',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'web-component';
+  isElementDisplayed1 = true;
+  isElementDisplayed2 = true;
+  isElementDisplayed3 = true;
+
+  toggle1() {
+    this.isElementDisplayed1 = !this.isElementDisplayed1;
+    console.log(this.isElementDisplayed1);
+  }
+
+  toggle2() {
+    this.isElementDisplayed2 = !this.isElementDisplayed2;
+  }
+
+  toggle3() {
+    this.isElementDisplayed3 = !this.isElementDisplayed3;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule, Injector} from '@angular/core';
+import {createCustomElement} from '@angular/elements';
+import { ElementZoneStrategyFactory } from 'elements-zone-strategy';
 
-import { AppComponent } from './app.component';
+
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
@@ -11,6 +14,18 @@ import { AppComponent } from './app.component';
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [],
+  entryComponents: [
+    AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+  }
+
+  ngDoBootstrap() {
+    const strategyFactory = new ElementZoneStrategyFactory(AppComponent, this.injector);
+    const el = createCustomElement(AppComponent, {injector: this.injector, strategyFactory});
+    customElements.define('greet-comp', el);
+  }
+}
