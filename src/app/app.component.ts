87cd9698cb4d37720e71febd5c0f087300890db9
Avram Virgil <av.virgil@gmail.com>
2018-11-29 14:39:49 +0000

    added input to web element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, OnChanges, Output, SimpleChanges, Input} from '@angular/core';
 
 @Component({
   selector: 'greet-comp',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'web-component';
+export class AppComponent implements OnChanges {
+  title = 'This is a web element with input';
+
+  @Input() label = '';
+  @Output() action = new EventEmitter<number>();
   isElementDisplayed1 = true;
   isElementDisplayed2 = true;
   isElementDisplayed3 = true;
+  private numberOfClicks = 0;
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes);
+  }
+
+  handleClick(event) {
+    this.numberOfClicks++;
+    this.action.emit(this.numberOfClicks);
+  }
 
   toggle1() {
     this.isElementDisplayed1 = !this.isElementDisplayed1;
